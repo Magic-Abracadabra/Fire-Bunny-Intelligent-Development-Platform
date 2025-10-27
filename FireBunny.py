561f2cf9515c4e8a7b4b9ae454314e4de23fea22
如意The Magic Wand <you2kai1yuan2@hainanu.edu.cn>
2025-10-27 18:16:54 +0800

    Update FireBunny.py

--- FireBunny.py
+++ FireBunny.py
@@ -2,7 +2,7 @@ from fbidp import *
 import sys
 
 if len(sys.argv)==2:
-	with open(sys.argv[1]) as 秘籍:
+	with open(sys.argv[1], encoding='utf-8') as 秘籍:
 		咒语 = 秘籍.read()
 	try:
 		执行_(咒语)
@@ -10,4 +10,5 @@ if len(sys.argv)==2:
 		pag.alert('【ToT】', '火小兔')
 	秘籍.close()
 else:
+
 	sys.exit()
